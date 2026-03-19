0e328bcdddca6630d8e07f758691eebd14a31dbf
ThiagoRuiz2026 <thiagomauricioruiz3@gmail.com>
2026-03-19 12:04:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Ejemlo 1");
+int a = 25;
+int b = 30;
+if (a > b)
+{
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 2");
+int c = 14;
+int d = 14;
+if (c <= d)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 3");
+int e = 0;
+int f = 0;
+if (0 == 0)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 4");
+int g = 10;
+int h = 10;
+if (10 != 10)
+{
+    Console.WriteLine("verdadero");
+
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 5");
+int i = 8;
+int j = 3;
+if (8 >= 3)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 6");
+int k = 15;
+int l = 15;
+if(15 < 15)
+{
+    Console.WriteLine("Verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 7");
+int m = 100;
+int n = 50;
+if(100 != 50)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 8");
+int ñ = 7;
+int o = 7;
+if(7 == 7)
+{
+    Console.WriteLine("verdadeero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 9");
+float p = 0.5f;
+int q = 0;
+if (0.5f > 0)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 10");
+int t = -5;
+int s = -2;
+if(-5 < -2)
+{
+    Console.WriteLine("verdadero");
+}
+Console.WriteLine("Ejemplo 1c");
+
+if (5 > 3 && 10 < 20)
+{
+    Console.WriteLine("verdadero");
+}
+else {
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 2c");
+if (8 == 10 || 5 != 2)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 3c");
+if (!(10 == 10))
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 4c");
+if (15 > 10 && !(5 < 10))
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 5c");
+if (10 + 2 > 10 || 5 * 2 < 8)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 6c");
+int temperatura = 35;
+bool humedad = false;
+if (temperatura > 30 && humedad == true)
+{
+    Console.WriteLine("verdadero");
+}
+else
+{
+    Console.WriteLine("falso");
+}
+Console.WriteLine("Ejemplo 7c");
