0d7fd6a44806ae0097bdd08ebc9a583dea1a3c24
ThiagoRuiz2026 <thiagomauricioruiz3@gmail.com>
2026-04-23 09:07:47 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,72 @@
-﻿using System.Diagnostics.CodeAnalysis;
-Console.WriteLine("Ejercicio1");
-for (int x = 0; x <= 10; x++){
-    Console.WriteLine(x);
-}
-Console.WriteLine("Ejercicio2");
-for (int y = 0; y <= 5; y++)
-{
-    Console.WriteLine("Hola");
-}
-Console.WriteLine("Ejercicio3");
-for (int z = 0; z <= 20; z += 2)
-{
-    Console.WriteLine(z);
-}
-Console.WriteLine("Ejercicio4");
-for (int a = 7; a <= 70; a += 7)
-{
-    Console.WriteLine(a);
-    
-}
-Console.WriteLine("Ejercicio 5");
-int suma = 0;
-for (int b = 1; b <= 5; b++)
-{
-suma += b;
-}
+﻿//1
 
-Console.Write("1+2+3+4+5= ");
-Console.Write(suma);
+Console.WriteLine("ingrese una palabra para contar las vocales");
+string frase = Console.ReadLine();
+
+int contador = 0;
+for (int i = 0; i < frase.Length; i++)
+{
+    if ("aeiouAEIOU".Contains(frase[i]))
+    {
+        contador++;
+    }
+}
+Console.WriteLine("cantidad de vocales: " + contador);
+
+//2
+
+Console.WriteLine("Ingrese una palabra para invertirla");
+
+string palabra = Console.ReadLine();
+string invertido = "";
+
+foreach (char letra in palabra)
+{
+    invertido = letra + invertido;
+}
+Console.WriteLine("Texto invertido: " + invertido);
+
+//3
+
+Console.WriteLine("ingrese un numero entero");
+string numero = Console.ReadLine();
+int suma = 0;
+foreach (char digito in numero)
+{
+    suma += (digito - '0');
+}
+Console.WriteLine("resultado: " + suma);
+
+//4
+
+Console.WriteLine("Ingrese un texto: ");
+string texto = Console.ReadLine();
+Console.WriteLine("Palabra prohibida: ");
+string prhibida = Console.ReadLine();
+Console.WriteLine("palabra de remplazo: ");
+string remplazo = Console.ReadLine();
+string palabraActual = "";
+string resultad = texto.Replace(prhibida, remplazo);
+Console.WriteLine("resultado: " + resultad);
+
+//5
+
+Console.WriteLine("nombre: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Apellido: ");
+string apellido = Console.ReadLine();
+
+Console.WriteLine("Iniciales: " +
+    nombre[0] + "." + apellido[0] + ".");
+Console.WriteLine("nombre: " + nombre);
+Console.WriteLine("apellido: " + apellido);
+
+//6
+
+Console.WriteLine("Palabra:");
+string p = Console.ReadLine(), inv = "";
+
+foreach (char c in p)
+    inv = c + inv;
+Console.WriteLine(p == inv ? "Es palindromo" : "No es palindromo");
