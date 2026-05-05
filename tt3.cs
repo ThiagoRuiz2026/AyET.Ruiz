5e8ce732295a535bad1656a1ae63547340344165
ThiagoRuiz2026 <thiagomauricioruiz3@gmail.com>
2026-05-05 11:24:57 -0300

    Add files via upload

---
+++ tt3.cs
@@ -0,0 +1,24 @@
+﻿Int A1 = 12 + 8 * 3;
+Console.Writeline(A1);
+
+Int A2 = (12 + 8) * 3;
+Console.Writeline(A2);
+
+Int A3 = 50 / 5 + 4;
+Console.Writeline(A3);
+
+Int A4 = 100 - 10 * 2;
+onsole.Writeline(A4);
+
+Int A5 = 15 % 2;
+onsole.Writeline(A5);
+
+Int A6 = 20 % 5;
+onsole.Writeline(A6);
+
+Int A8 = 10 / 2 * 4;
+onsole.Writeline(A8);
+
+Int A10 = 5 + 5 * 10 - 2;
+onsole.Writeline(A10);
+
