3c4d2981898b24aedf386c3e642b63245b85219f
ThiagoRuiz2026 <thiagomauricioruiz3@gmail.com>
2026-05-05 11:34:21 -0300

    Add files via upload

---
+++ tt6.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Ingrese 0 o 1:");
+
+int num;
+
+while (!int.TryParse(Console.ReadLine(), out num) || (num != 0 && num != 1))
+{
+    Console.WriteLine("Error. Ingrese 0 o 1:");
+}
+
+
+// ----------------------------
+
+Console.WriteLine("Ingrese un número de 2 cifras:");
+
+int num2;
+
+while (!int.TryParse(Console.ReadLine(), out num2) || num2 < 10 || num2 > 99)
+{
+    Console.WriteLine("Error. Ingrese un número de 2 cifras:");
+}
+
